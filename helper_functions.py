93cafdc3f33349c6028f385deae872a857a8c6f2
Kristina Barounis <krbarounis@gmail.com>
2019-10-03 19:23:20 -0400

    added functions to helper file, renamed EDA file

--- helper_functions.py
+++ helper_functions.py
@@ -72,3 +72,58 @@ def finalize_token(reviews):
         lemmas = lem_words(tokens,lemmatizer)
         corpus.append(lemmas)
     return corpus
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    plot_confusion_matrix(cm, classes,normalize=False,
+                          title='Confusion matrix',cmap=plt.cm.Blues)
+    
+    """
+    #Add Normalization Option
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('confusion')
+    
+def plot_AUC_ROC(y_score,fpr,tpr):
+    sns.set_style("darkgrid", {"axes.facecolor": ".9"})
+    print('AUC: {}'.format(auc(fpr, tpr)))
+    plt.figure(figsize=(10,8))
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve')
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.yticks([i/20.0 for i in range(21)])
+    plt.xticks([i/20.0 for i in range(21)])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.show()
